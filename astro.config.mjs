0acbceba2da45efb91ba426e86a3983f3dd30dc2
Daniel-JA03 <danieljaimesamancio@gmail.com>
2026-04-30 19:28:43 -0500

    fix: rutas de assets sin BASE_URL para netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://Daniel-JA03.github.io",
-  base: "/portafolio"
+  base: "/"
 });
